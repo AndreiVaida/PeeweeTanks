26614724c5e64e938a6a32e6bf5f85f7a6a9173c
AndreiVaida <andrei_vd2006@yahoo.com>
2019-03-21 13:25:03 +0200

    Made HomePage.

---
+++ src/HomePage.js
@@ -0,0 +1,18 @@
+class HomePage {
+    constructor() {
+        console.log('HomePage - constructor');
+    }
+
+    preload() {
+        this.game.load.image('homeBackground', 'images/LowPolyMountain.jpg');
+        this.game.load.image('playButton', 'images/PlayButtonBlue.png');
+    }
+
+    create() {
+        console.log('HomePage - create');
+        var homeBackground = this.game.add.sprite(0, 0, "homeBackground");
+        var playButton = this.game.add.button(gameWidth/2, gameHeight/2, "playButton", this.playTheGame, this);
+        playButton.anchor.setTo(0.5, 0.5);
+    }
+
+}
