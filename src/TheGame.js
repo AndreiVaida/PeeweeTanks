8d0037128a4e585b946621096a349a5efd29c784
AndreiVaida <andrei_vd2006@yahoo.com>
2019-03-25 23:11:31 +0200

    Fire cannons with mouse and explosion. Cannonballs and explosions automatically deletes.

--- src/TheGame.js
+++ src/TheGame.js
@@ -5,10 +5,10 @@ class TheGame {
         this.land = null;
         this.tankBody = null;
         this.tankTurret = null;
+        this.bullets = [];
         this.collisionGroup = null;
         this.cursors = null;
         this.spaceKey = null;
-        this.spaceIsDown = false;
     }
 
     preload() {
@@ -17,6 +17,7 @@ class TheGame {
         this.game.load.image('tankBody', 'images/tankBody.png');
         this.game.load.image('tankTurret', 'images/tankTurret.png');
         this.game.load.image('cannonBullet', 'images/CannonBullet.png');
+        this.game.load.image('explosion', 'images/Explosion1.gif');
     }
 
     create() {
@@ -43,27 +44,63 @@ class TheGame {
         // controls
         this.cursors = this.game.input.keyboard.createCursorKeys();
         this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        this.aKey = this.game.input.keyboard.addKey(Phaser.Keyboard.A);
+        this.dKey = this.game.input.keyboard.addKey(Phaser.Keyboard.D);
+        this.rKey = this.game.input.keyboard.addKey(Phaser.Keyboard.R);
+
+        this.background.inputEnabled = true;
+        this.background.events.onInputDown.add(this.clickListener, this);
+    }
+
+    createBullet() {
+        let bullet = this.add.sprite(this.tankBody.x + 30, this.tankBody.y - 10, 'cannonBullet');
+        this.game.physics.arcade.enable(bullet);
+        bullet.scale.setTo(0.2, 0.2);
+        bullet.body.bounce.y = 0.3;
+        bullet.body.gravity.y = 1000;
+        bullet.body.collideWorldBounds = false;
+        return bullet;
     }
 
     update() {
+        // collision
         this.game.physics.arcade.collide(this.tankBody, this.collisionGroup);
+        this.bullets.forEach((bullet, index, list) => {
+            if (bullet.body.x < 0 || bullet.body.x > gameWidth || bullet.body.y < 0) {
+                list.splice(index, 1);
+                return;
+            }
+            const hitLand = this.game.physics.arcade.collide(bullet, this.collisionGroup);
+            const hitTank = this.game.physics.arcade.collide(bullet, this.tankBody);
+            if (hitLand || hitTank) {
+                this.explode(bullet);
+                if (hitTank) {
+                    this.giveDamage();
+                }
+                list.splice(index, 1);
+            }
+        });
 
+        // tank movement
         this.tankBody.body.velocity.x = 0;
-        if (this.cursors.left.isDown) {
+        if (this.cursors.left.isDown || this.aKey.isDown) {
             this.tankBody.body.velocity.x = -100;
-        } else if (this.cursors.right.isDown) {
+        } else if (this.cursors.right.isDown || this.dKey.isDown) {
             this.tankBody.body.velocity.x = 100;
         }
-        if (this.cursors.up.isDown && this.tankBody.body.touching.down) {
+        if (this.spaceKey.isDown && this.tankBody.body.touching.down) {
             this.tankBody.body.velocity.y = -500;
         }
-        if (this.spaceKey.isDown && !this.spaceIsDown) {
-            this.spaceIsDown = true;
-            this.fire();
+        if (this.rKey.isDown) {
+            this.resetTankPosition()
         }
-        if (this.spaceKey.isUp) {
-            this.spaceIsDown = false;
-        }
+
+        // turret rotation
+        this.tankTurret.rotation = this.game.physics.arcade.angleToPointer(this.tankBody);
+    }
+
+    clickListener() {
+        this.fire();
     }
 
     paused() {
@@ -75,6 +112,29 @@ class TheGame {
     }
 
     fire() {
-        console.log('fire');
+        let bullet = this.createBullet();
+        this.bullets.push(bullet);
+        const p = new Phaser.Point(this.tankTurret.x, this.tankTurret.y);
+        p.rotate(p.x, p.y, this.tankTurret.rotation, false, 34);
+        this.physics.arcade.velocityFromRotation(this.tankTurret.rotation, 1000, bullet.body.velocity);
+    }
+
+    resetTankPosition() {
+        this.tankBody.reset(40, 550);
+    }
+
+    explode(bullet) {
+        const x = bullet.x - 90;
+        const y = bullet.y - 180;
+        bullet.destroy();
+        const explosion = this.add.sprite(x, y, 'explosion');
+        this.add.tween(explosion).to( { alpha: 0 }, 500, "Linear", true);
+        setTimeout(() => {
+            explosion.destroy();
+        }, 500);
+    }
+
+    giveDamage() {
+
     }
 }
